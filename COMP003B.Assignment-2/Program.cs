7b4478b09247f4d04eb85a5d129da02eba735e49
mattbzca <matthewtan@my.whccd.edu>
2025-03-26 18:12:42 -0700

    Add project files.

---
+++ COMP003B.Assignment-2/Program.cs
@@ -0,0 +1,49 @@
+/*
+ * Author: Matthew Tan
+ * Course: COMP-003B: ASP.NET Core
+ * Faculty name: Jonathan Cruz
+ * Purpose: This is my ASP.NET Core MVC web application.
+ * 
+ */
+
+namespace COMP003B.Assignment_2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+            app.UseMiddleware<COMP003B.Assignment_2.Middleware.RequestLoggingMiddleware>();
+
+            app.UseWelcomePage("/welcome");
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
